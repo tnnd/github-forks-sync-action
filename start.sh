713b1cc5cc3cf40179708a1a67ade637c59c4f08
tnnd <37441897+tnnd@users.noreply.github.com>
2021-04-06 14:33:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -21,18 +21,21 @@ echo "Synchronizing repostiory ${TARGET_REPOSITORY}:${INPUT_TARGET_BRANCH} with 
     exit 1;
 };
 
-if ${INPUT_FORCE}; then
-    _FORCE_OPTION='--force'
-fi
+//if ${INPUT_FORCE}; then
+//    _FORCE_OPTION='--force'
+//fi
 
 if ${INPUT_TAGS}; then
     _TAGS='--tags'
 fi
 
 upstream_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_UPSTREAM_REPOSITORY}.git"
-upstream_dir=${INPUT_UPSTREAM_REPOSITORY##*/}
+//upstream_dir=${INPUT_UPSTREAM_REPOSITORY##*/}
 target_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${TARGET_REPOSITORY}.git"
+target_dir=${TARGET_REPOSITORY##*/}
 
-git clone ${upstream_repo}
-cd ${upstream_dir}
-git push $_FORCE_OPTION --follow-tags $_TAGS ${target_repo} ${INPUT_UPSTREAM_BRANCH}:${INPUT_TARGET_BRANCH}
+git clone ${target_repo}
+cd ${target_dir}
+git remote add upstream ${upstream_repo}
+git pull upstream master
+git push
